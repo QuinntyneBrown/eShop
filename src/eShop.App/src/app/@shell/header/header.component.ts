900a1ba26b1ef3917fb6a6c8f2f3edd772232531
QuinntyneBrown <quinntynebrown@gmail.com>
2021-04-08 00:44:08 -0400

    update

--- src/eShop.App/src/app/@shell/header/header.component.ts
+++ src/eShop.App/src/app/@shell/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  styleUrls: ['./header.component.scss'],
+  host: {
+    'class':'g_layout_container'
   }
+})
+export class HeaderComponent {
+  @Output() searchClick: EventEmitter<any> = new EventEmitter();
 
+  @Output() menuClick: EventEmitter<any> = new EventEmitter();
+
+  @Output() profileClick: EventEmitter<any> = new EventEmitter();
+
+  @Output() shoppingCartClick: EventEmitter<any> = new EventEmitter();
 }
